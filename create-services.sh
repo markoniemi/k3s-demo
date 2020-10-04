036b4657247ec5a58dfde78a3242624804774ff7
Marko Niemi <marko.j.niemi@gmail.com>
2020-10-04 16:04:19 +0300

    Several fixes.

--- create-services.sh
+++ create-services.sh
@@ -3,5 +3,3 @@ kubectl apply -f user-repository/user-repository.yaml
 
 k3d image import markoniemi/user-application:latest
 kubectl apply -f user-application/user-application.yaml
-kubectl apply -f user-application/user-application-ingress.yaml
-#kubectl port-forward service/user-application 8081:8083
