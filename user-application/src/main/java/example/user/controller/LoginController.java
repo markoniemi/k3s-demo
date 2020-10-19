4524a9079b100029de221e85e2c2149e47b99422
Marko Niemi <marko.j.niemi@gmail.com>
2020-10-19 20:51:08 +0300

    Log logins.

--- user-application/src/main/java/example/user/controller/LoginController.java
+++ user-application/src/main/java/example/user/controller/LoginController.java
@@ -3,10 +3,14 @@ package example.user.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import lombok.extern.log4j.Log4j2;
+
 @Controller
+@Log4j2
 public class LoginController {
     @GetMapping(value = "/login")
     public String home() {
+    	log.info("/login");
         return "/login";
     }
 }
