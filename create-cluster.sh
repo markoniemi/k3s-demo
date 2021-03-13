10165d04da9328cb4c199874b7b1b7f0e03354f5
Marko Niemi <marko.j.niemi@gmail.com>
2021-03-13 15:49:34 +0200

    Use k3d config file. Remove Portainer, use Lens instead.

--- create-cluster.sh
+++ create-cluster.sh
@@ -1,32 +1,19 @@
 echo create cluster
-k3d cluster create k3s-default \
-  -i docker.io/rancher/k3s:v1.20.4-k3s1 \
-  --servers 1 \
-  --k3s-server-arg "--no-deploy=traefik" \
-  -p 80:80@loadbalancer \
-  -p 8500:8500@loadbalancer \
-  -p 9000:9000@loadbalancer \
-  -p 8080:8080@loadbalancer \
-  -p 8082:8082@loadbalancer 
-echo set kubeconfig
-k3d kubeconfig merge k3s-default --kubeconfig-switch-context
+k3d cluster create --config k3d-default.yaml
 
 echo create user
 kubectl apply -f default-user-role.yaml
 
 echo add repos
+#helm repo for traefik 
 helm repo add traefik https://helm.traefik.io/traefik
-helm repo add portainer https://portainer.github.io/k8s/
+#helm repo for postgresql 
 helm repo add bitnami https://charts.bitnami.com/bitnami
 helm repo update
 
 echo install Traefik
 helm install traefik traefik/traefik --set image.tag=v2.4.2
 kubectl apply -f traefik-ingress.yaml
-
-echo install Portainer
-kubectl create namespace portainer
-helm install -n portainer portainer portainer/portainer --set service.type=LoadBalancer
 
 # Postgre
 echo install postgresql in cluster
--- demo.sh
+++ demo.sh
@@ -1,5 +1,5 @@
 set -e
 ./create-cluster.sh
-kubectl wait --for=condition=ready --timeout=3600s pod -l app.kubernetes.io/instance=ingress-nginx
+kubectl wait --for=condition=ready --timeout=3600s pod -l app.kubernetes.io/instance=traefik
 ./user-repository.sh
 ./user-application.sh
