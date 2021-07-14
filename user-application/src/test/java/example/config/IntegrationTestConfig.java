bf2d8083e7142db209a9d16636cbf52080247a19
Marko Niemi <marko.j.niemi@gmail.com>
2021-07-14 08:02:37 +0300

    Fix for tests, update maven exec plugin.

--- user-application/src/test/java/example/config/IntegrationTestConfig.java
+++ user-application/src/test/java/example/config/IntegrationTestConfig.java
@@ -1,5 +1,6 @@
 package example.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
@@ -8,4 +9,8 @@ import example.selenium.SeleniumConfig;
 @Configuration
 @Import({  SeleniumConfig.class })
 public class IntegrationTestConfig {
+    @Bean
+    public String url() {
+        return "http://localhost:8083/users";
+    }
 }
