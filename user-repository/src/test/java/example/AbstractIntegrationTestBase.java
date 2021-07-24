b226fdd35a0306fe7679cdbd00fd021fedaa68e6
Marko Niemi <marko.j.niemi@gmail.com>
2021-07-24 08:19:32 +0300

    Remove SystemEndpoint.

--- user-repository/src/test/java/example/AbstractIntegrationTestBase.java
+++ user-repository/src/test/java/example/AbstractIntegrationTestBase.java
@@ -2,24 +2,14 @@ package example;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import java.net.http.HttpClient;
-import java.util.Collections;
-
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.autoconfigure.http.HttpMessageConvertersAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.cloud.openfeign.FeignAutoConfiguration;
 import org.springframework.core.env.Environment;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.ContextHierarchy;
@@ -27,7 +17,6 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import config.IntegrationTestConfig;
 import example.repository.UserRepositoryApplication;
-import lombok.extern.log4j.Log4j2;
 
 /**
  * Base class for integration tests, enables running multiple tests
@@ -38,7 +27,6 @@ import lombok.extern.log4j.Log4j2;
 @SpringBootTest(classes = UserRepositoryApplication.class, webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @ContextHierarchy(@ContextConfiguration(classes = IntegrationTestConfig.class))
 @ActiveProfiles("h2")
-@Log4j2
 @ImportAutoConfiguration({ FeignAutoConfiguration.class, HttpMessageConvertersAutoConfiguration.class })
 public class AbstractIntegrationTestBase {
     @Autowired
--- user-repository/src/test/java/example/RestClient.java
+++ user-repository/src/test/java/example/RestClient.java
@@ -11,20 +11,21 @@ public class RestClient {
     public static String get(String url) {
         return get(url, HttpStatus.OK);
     }
-    
+
     public static String get(String url, HttpStatus httpStatus) {
         return execute(HttpMethod.GET, url, httpStatus);
     }
+
     public static String post(String url) {
         return post(url, HttpStatus.OK);
     }
-    
+
     public static String post(String url, HttpStatus httpStatus) {
         return execute(HttpMethod.POST, url, httpStatus);
     }
+
     private static String execute(HttpMethod httpMethod, String url, HttpStatus httpStatus) {
-        ResponseEntity<String> response = new TestRestTemplate().exchange(url, httpMethod,
-                null, String.class);
+        ResponseEntity<String> response = new TestRestTemplate().exchange(url, httpMethod, null, String.class);
         assertTrue(response.getStatusCode() == httpStatus);
         return response.getBody();
     }
